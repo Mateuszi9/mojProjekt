de71873b766cd79fa006927c18f2d7bb0b5cc606
Mateusz Izdebski <mati2k51@gmail.com>
2022-11-05 16:54:48 +0100

    Lesson-03-Create_react_app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">TwojeKorki</a>
+      </header>
+      <main>
+        list announcements
+      </main>
+    </div>
+  );
+}
+
+export default App;
