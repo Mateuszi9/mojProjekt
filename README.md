75e41f7659f6decd89bb98c97330061d4777e255
Mateusz Izdebski <mati2k51@gmail.com>
2022-11-05 21:37:43 +0100

    Lesson-06-Add-Bootstrap-To-AppJS

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
     1. npm i react-router-dom
     2. create route for home screen
     3. create router for announcement screen
+7. Add bootstrap UI Framework
+    1. npm install react-bootstrap bootstrap react-router-bootstrap
+    2. update App.js
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import AnnouncementScreen from './screens/AnnouncementScreen';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to="/">TwojeKorki</Link>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>TwojeKorki</NavBar.Brand>
+              </LinkContainer>
+            </Container>
+          </NavBar>
         </header>
         <main>
-          <Routes>
-            <Route path="/announcement/:_id" element={<AnnouncementScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route
+                path="/announcement/:_id"
+                element={<AnnouncementScreen />}
+              />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
